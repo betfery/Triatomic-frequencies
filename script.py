293c55754991415921f39e72d44f0a999af2e84f
unknown <rc688@cam.ac.uk>
2018-10-09 14:29:17 +0100

    Added lines that does not work on windows, trying linux

---
+++ script.py
@@ -0,0 +1,17 @@
+"""To be added
+
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    PATH = "/H2Ooutfiles"
+    for file in os.listdir(PATH):
+        print(file)
+
+
+if __name__ == '_main_':
+    main()
